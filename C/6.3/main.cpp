d24bc6e1db6fc130adcd70d08e025a109c7cf009
RoyFrost <972469981@qq.com>
2019-11-11 18:13:01 +0800

    finish C 6.3

--- C/6.3/main.cpp
+++ C/6.3/main.cpp
@@ -2,15 +2,22 @@
 #include <string.h>
 #include <string>
 int main(){
-    char *input = (char*)malloc(10*sizeof(char));
+    printf("plz input a string shorter than 100 characters\n");
+    char *input = (char*)malloc(100*sizeof(char));
     gets(input);
     int len=strlen(input);
+    if (len > 100) {
+        printf("too longer!\n");
+        return 0;
+    }
     printf("len=%d\n",len);
     for (int i = 0; i < len; i++) {
         //if (input[i]== ' ') printf(" ");
         printf("%c",input[i]);
     }
+    free(input);
+    //printf("i=%d\n",i);
     /*char *storage=*(char**)((int)__iob_func()+8);
     printf("%s", storage);*/
-    return 0;
+    return 1;
 }
